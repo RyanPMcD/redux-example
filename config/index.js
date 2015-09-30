86dc7080eaf3b71bcbb37e34387b30bd1accd835
RyanPMcD <ryan.patrick.mcdonald@gmail.com>
2015-09-30 00:21:24 -0400

    file names are hard...

--- config/index.js
+++ config/index.js
@@ -1,18 +1,7 @@
 'use strict';
-import Hapi from 'hapi';
-import config from './config';
 
-let server = new Hapi.Server();
-
-server.connection(config.server);
-console.log('info', 'Server started.');
-server.start(function (err) {
-    // After method already executed
-    console.log('Server starting @' + server.info.uri);
-    console.log('Server started.');
-    if (err) {
-        console.log(err);
-    }
-});
-
-module.exports = server;
+export default {
+  server: {
+    port: 3000
+  }
+};
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+import Hapi from 'hapi';
+import config from './config';
+
+let server = new Hapi.Server();
+
+server.connection(config.server);
+
+server.start(function (err) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Server starting @' + server.info.uri);
+});
+
+module.exports = server;
